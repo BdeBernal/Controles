d13cec6d83a34e77bb68bdcdbbb253a1ad01e7b3
BdeBernal <a23BernalRC@iessanclemente.net>
2023-10-27 12:35:53 +0200

    Modificación para recogida por teclado

--- raiz.py
+++ raiz.py
@@ -1,10 +1,9 @@
 # Python Program to calculate the square root
 
 # Note: change this value for a different result
-num = 8
+num = float(input("Enter a number: "))
 
 # To take the input from the user
 #num = float(input('Enter a number: '))
 
-num_sqrt = num ** 0.5
-print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+print("The square root of " f"{num} is {round(num ** 0.5)}")
